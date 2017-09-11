8566c7dee12f40d40e57931845670cc47fd87669
adityarao310 <hello@adityarao.name>
2017-09-12 01:30:56 +0530

    Solved for match issues!

--- js/app.js
+++ js/app.js
@@ -36,16 +36,79 @@ $.each(shuffleList, function(index, value) {
     $(".deck").append(print);
 });
 
-// open = add child to array + change class to open show
-// 1st click.. open. 2nd click.. open. 3rd click.. open.
-// on 3rd click --> check if 1st two are equal
-//    If equal --> change class to match, add to match, remove from array
-//    if not equal (to first two) --> remove from array, change class to card
-// When matchlist reaches 16 = everything unclickable + show new HTML
+// on 1st click.. change class and push to open list
+// on 2nd click.. change class, push to open list AND
+// if openList length = 2, check for match via below >
+//          if no match.. hold for 1 sec, change class back and empty openList
+//          if match.. change class to match, add to matchlist and empty openList
+var openList = [];
+var matchList = [];
+$('.card').click(function() {
+    var clicked = $(this);
+    if (openList.length < 2) {
+        var cardOpen = clicked.toggleClass('open show').children();
+        openList.push(cardOpen);
+    } else if (openList.length === 2) {
+        // check for match if ($(this).hasClass("lol"))
+        a = openList[0].attr('class');
+        b = openList[1].attr('class');
+        if (a === b) {
+            $(openList[0]).parent().toggleClass('match');
+            $(openList[1]).parent().toggleClass('match');
+            matchList.push(openList[0]);
+            matchList.push(openList[1]);
+            openList = [];
+        } else {
+            $(openList[0]).parent().removeClass('open show');
+            $(openList[1]).parent().removeClass('open show');
+            openList = [];
+        }
+    }
+
+    if (matchList.length === 16) {
+        alert("Game is done! You win");
+    }
+});
+
+
+
+
+/*
 var openList = [];
 var matchList = [];
 $(".card").click(function() {
-    // reveal the card type and store it into empty list
+    var clicked = $(this);
+    if (openList.length < 2) {
+        // store child of card class and push to openList
+        if (!matchList.includes(clicked)) {
+            var cardOpen = clicked.toggleClass('open show').children();
+            openList.push(cardOpen);
+        }
+    } else if (openList.length === 2) {
+        // check if the two stored things match
+        if (openList[0] === openList[1]) {
+            // add to matchList, change class to match, empty openList
+            $(openList[0]).toggleClass('match');
+            $(openList[1]).toggleClass('match');
+            matchList.push(openList[0]);
+            matchList.push(openList[1]);
+            openList = [];
+        } else {
+            // remove open show after .5 secs and empty openlist
+            setTimeout(function() {
+                $(openList[0]).parent().removeClass('open show');
+                $(openList[1]).parent().removeClass('open show');
+                openList = [];
+            }, 300);
+        }
+
+    } else if (matchList.length === 16) {
+        alert("Game is done! You win");
+    }
+});
+*/
+
+/*
     var cardOpen = $(this).toggleClass('open show').children();
     openList.push(cardOpen);
     // console.log(openList);
@@ -72,3 +135,4 @@ $(".card").click(function() {
         alert("Game is done! You win");
     }
 });
+*/
